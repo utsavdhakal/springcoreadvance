35debceaf96b5d291df434360312b0accb5866bd
Utsav Dhakal <udhakal970@gmail.com>
2022-11-14 13:53:31 +0545

    autowiring by constructor

--- src/main/java/io/coffeecode/springadvance/autowiring/Employee.java
+++ src/main/java/io/coffeecode/springadvance/autowiring/Employee.java
@@ -3,6 +3,10 @@ package io.coffeecode.springadvance.autowiring;
 public class Employee {
     private Address address;
 
+    public Employee(Address address) {
+        this.address = address;
+    }
+
     public Address getAddress() {
         return address;
     }
